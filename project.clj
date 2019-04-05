0b01f2568a7b9d3db6005275152b276d7c983c44
henrik42 <heineh@msgn08198.int.root.msg.ag>
2019-04-05 10:49:52 +0200

    use Buttle 1.0.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject use-buttle "0.1.0-SNAPSHOT"
   :description "A simple usage example for Buttle https://github.com/henrik42/buttle"
   :url "https://github.com/henrik42/use-buttle"
-  :dependencies [[buttle "0.1.1-SNAPSHOT"]
+  :dependencies [[buttle "1.0.0"]
                  [org.postgresql/postgresql "9.4.1212"]]
   :main use-buttle.core
   :profiles {:uberjar {:aot :all}})
